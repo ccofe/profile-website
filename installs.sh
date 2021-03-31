927a7ab01dba92c98a101b93953d3d2cdfc5b47f
ccofe <ccofelice@gmail.com>
2021-03-31 09:08:17 +0000

    added linux batch files for nodejs and python installs

---
+++ installs.sh
@@ -0,0 +1,26 @@
+cd frontend
+
+npm init -y
+
+npm i webpack webpack-cli --save-dev
+
+npm i @babel/core babel-loader @babel/preset-env @babel/preset-react --save-dev
+
+npm i react react-dom --save-dev
+
+npm install @material-ui/core
+
+npm install @babel/plugin-proposal-class-properties
+
+npm install react-router-dom
+
+npm install @material-ui/icons
+
+npm install --save-dev style-loader
+
+npm install --save-dev css-loader
+
+echo "================================================"
+echo "		NPM Installs Complete !!!   "
+echo "================================================"
+
---
+++ python_installs.sh
@@ -0,0 +1,9 @@
+pip3 install wheel
+pip3 install django
+pip3 install django-admin
+pip3 install djangorestframework
+pip3 install django-cors-headers
+pip3 install pillow
+pip3 install psycopg2-binary
+pip3 install gunicorn
+
