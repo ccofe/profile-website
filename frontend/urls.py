23336baebf18f42f26fe6806813941774da24246
ccofe <ccofelice@gmail.com>
2021-04-07 04:56:45 -0400

    testing a hello world url

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, homePageView
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('hello', homePageView)
 ]
