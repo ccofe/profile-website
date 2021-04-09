c1aeeb03688aa207f456549c4cca11bfee32f674
ccofe <ccofelice@gmail.com>
2021-04-09 12:49:57 -0400

    updated models to TextFields

--- api/models.py
+++ api/models.py
@@ -5,15 +5,15 @@ class Job(models.Model):
     company = models.CharField(max_length=100)
     image = models.ImageField(upload_to='')
     title = models.CharField(max_length=100)
-    summary = models.CharField(max_length=600)
+    summary = models.TextField()
     dates = models.CharField(max_length=100)
 
 class Me(models.Model):
-    about = models.CharField(max_length=200)
+    about = models.TextField()
     picture = models.ImageField(upload_to='')
     resume = models.FileField(upload_to='')
     firstName = models.CharField(max_length=20)
     lastName = models.CharField(max_length=20)
 
 class Site(models.Model):
-    about = models.CharField(max_length=200)
+    about = models.TextField(max_length=200)
