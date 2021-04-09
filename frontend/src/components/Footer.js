3c01e0bf3d907bf177e37730a1e802c2f44012e6
ccofe <ccofelice@gmail.com>
2021-04-09 12:06:41 -0400

    moved the github and linkedin links to the top

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -25,12 +25,7 @@ export default function Footer(props) {
           
           <p class='footer__aboutme' onClick={onAboutMe}>About Me</p>
           <p class='footer__aboutsite' onClick={onAboutSite}>About This Page</p>
-          <a href="https://github.com/ccofe" target="_blank" rel="noopener noreferrer">
-            <i class="fa fa-github fa-2x github-icon"></i>
-          </a>
-          <a href="https://www.linkedin.com/in/christopher-cofelice-8080262a" target="_blank" rel="noopener noreferrer">
-            <i class="fa fa-linkedin-square fa-2x linkedin-icon"></i>
-          </a>
+          
       </div>
     );
 
--- frontend/src/components/TopSection.js
+++ frontend/src/components/TopSection.js
@@ -7,8 +7,14 @@ export default function TopSection(props) {
             <div class='intro__shade'></div>
             <h1>Chris Cofelice</h1>
             {/* <img src={props.profile.picture} class='profile-pic'/> */}
+            <a href="https://github.com/ccofe" target="_blank" rel="noopener noreferrer">
+                <i class="fa fa-github fa-2x github-icon"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/christopher-cofelice-8080262a" target="_blank" rel="noopener noreferrer">
+                <i class="fa fa-linkedin-square fa-2x linkedin-icon"></i>
+            </a>
             <a href={props.profile.resume} target="_blank" rel="noopener noreferrer">
-            <div class='resume'>Resume</div>
+                <div class='resume'>Resume</div>
             </a>
         </div>
     )
