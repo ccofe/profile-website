10aa30761188b9afa33e86092e7a56d0b0e7820d
ccofe <ccofelice@gmail.com>
2021-04-09 09:34:29 -0400

    update to models uploadto

--- api/models.py
+++ api/models.py
@@ -3,15 +3,15 @@ from django.db import models
 # Create your models here.
 class Job(models.Model):
     company = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='api/static/')
+    image = models.ImageField(upload_to='')
     title = models.CharField(max_length=100)
     summary = models.CharField(max_length=600)
     dates = models.CharField(max_length=100)
 
 class Me(models.Model):
     about = models.CharField(max_length=200)
-    picture = models.ImageField(upload_to='api/static/')
-    resume = models.FileField(upload_to='api/static/')
+    picture = models.ImageField(upload_to='')
+    resume = models.FileField(upload_to='')
     firstName = models.CharField(max_length=20)
     lastName = models.CharField(max_length=20)
 
